c1d48e95fd810200b7603bcd7cd3111eed9944ae
zhacies <abrahamgabalfin9a@gmail.com>
2025-10-03 21:30:08 +0800

    python password generator

---
+++ python/variables.py
@@ -0,0 +1,13 @@
+import random 
+string = "qwertyuoipasdfghjkl"
+numbers = "1234567890"
+operators = '%^&!#$'
+chars = string+ numbers+operators
+password = ""
+
+for x in range(15):
+  password += random.choice(chars)
+
+print(password)
+ 
+
