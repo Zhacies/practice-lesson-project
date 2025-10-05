3f9a51a4109b9605f03752987995908d7299d41c
zhacies <abrahamgabalfin9a@gmail.com>
2025-10-05 23:43:58 +0800

    re.sub syntax and explanation

--- python/palindromeIterative.py
+++ python/palindromeIterative.py
@@ -14,3 +14,15 @@ while left<right:
   right-=1
 
 print(True)
+
+
+hello = 'Hello, World 123'
+
+convert = re.sub(r'[A-Z 0-9]','',hello)  #syntax re.sub (r'[]','', variable)
+"""
+always remember that  ^ this thing is a exception 
+# if you don't put ^ A-Z and a-z 
+# different case if numbers if you put numbers they get delete 
+# unless use ^ 
+"""
+print(convert)
